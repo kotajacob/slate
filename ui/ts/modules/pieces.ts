d317c7b75e6622dba19f86346e8b5c6b71eb858c
Matthew W Eisenstein <dev@MEisenstein.ME>
2022-06-14 15:56:30 -0400

    Abstract game object information into GameObject class

--- ui/ts/modules/pieces.ts
+++ ui/ts/modules/pieces.ts
@@ -1,22 +1,19 @@
 import { Vector2, Rectangle } from "./area_tools";
 
 /**
- * Each Piece represents a game piece
+ * Parent cass for all game objects
+ * Pieces, Stacks, are considered game objects
  */
-export class Piece {
-	url: string;
-	position: Vector2;
-	w: number;
-	h: number;
-	boundary: Rectangle;
-	boundaryPath: Path2D;
-	image: HTMLImageElement;
+export abstract class GameObject {
+	position = new Vector2(0, 0);
+	boundary: Rectangle = {x: 0, y: 0, w: 0, h: 0};
+	boundaryPath = new Path2D();
+	image = new Image();
 
-	#imageIsLoaded: Promise<unknown> = Promise.resolve();
-	#wIsSet = Promise.resolve();
-	#hIsSet = Promise.resolve();
-	#boundaryWIsSet = Promise.resolve();
-	#boundaryHIsSet = Promise.resolve();
+	abstract url: string;
+	abstract w: number;
+	abstract h: number;
+	abstract boundaryPathGenerate(): void;
 
 	// Add this.x and this.y which reference this.position
 	get x() {
@@ -31,6 +28,21 @@ export class Piece {
 	set y(y: number) {
 		this.position.y = y;
 	}
+}
+
+/**
+ * Each Piece represents a game piece
+ */
+export class Piece extends GameObject {
+	url: string;
+	w: number;
+	h: number;
+
+	#imageIsLoaded: Promise<unknown> = Promise.resolve();
+	#wIsSet = Promise.resolve();
+	#hIsSet = Promise.resolve();
+	#boundaryWIsSet = Promise.resolve();
+	#boundaryHIsSet = Promise.resolve();
 
 	async boundaryPathGenerate() {
 		this.boundaryPath = new Path2D();
@@ -82,6 +94,7 @@ export class Piece {
 		wBoundary: number = w,
 		hBoundary: number = h,
 	) {
+		super();
 		this.url = url;
 		this.image = new Image();
 		this.image.src = this.url;
@@ -119,12 +132,8 @@ export class Piece {
  * Each Stack represents a stack of pieces, i.e. a deck of cards
  * To add or remove pieces from contents, use addChild and removeChild
  */
-export class Stack {
+export class Stack extends GameObject {
 	contents: (Piece|Stack)[];
-	position = new Vector2(0, 0);
-	boundary: Rectangle = {x: 0, y: 0, w: 0, h: 0};
-	boundaryPath = new Path2D();
-	image = new Image();
 
 	// Pass url through to first contents child
 	get url(): string {
@@ -137,20 +146,6 @@ export class Stack {
 		if (this.contents[0]) {
 			this.contents[0].url = url;
 		}
-	}
-
-	// Add this.x and this.y which reference this.position
-	get x() {
-		return this.position.x;
-	}
-	set x(x: number) {
-		this.position.x = x;
-	}
-	get y() {
-		return this.position.y;
-	}
-	set y(y: number) {
-		this.position.y = y;
 	}
 
 	// Pass (w)idth and (h)eight through to first contents child
@@ -187,13 +182,13 @@ export class Stack {
 	}
 
 	// Add methods for adding and removing children while updating parameters
-	addChild(child: (Piece|Stack), append: boolean = false) {
+	addChild(child: (Piece|Stack), append = false, index = 0) {
 		if (append) {
 			this.contents.push(child);
 			return;
 		}
-		this.contents.unshift(child);
-		this.#syncProperties();
+		this.contents.splice(index, 0, child);
+		if (index === 0) this.#syncProperties();
 	}
 	removeChild(index: number) {
 		this.contents.slice(index, 1);
@@ -219,6 +214,7 @@ export class Stack {
 	}
 
 	constructor(contents: (Piece|Stack)[]) {
+		super();
 		this.contents = contents;
 		this.#syncProperties();
 	}
